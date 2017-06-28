dffd9fd463dcb941a67dafbc00634e5d49b7dc65
Jonathan Fraine <exowanderer@users.noreply.github.com>
2017-06-28 16:55:10 -0400

    Generated Fake Data and Fixed Some Bugs

--- Fitting HAT-P-11b Kepler Transits.py
+++ Fitting HAT-P-11b Kepler Transits.py
@@ -8,7 +8,45 @@ h11Omega     = 360-162.149
 h11u1        = 0.646
 h11u2        = 0.048
 
-def batman_wrapper_raw(period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2, 
+def generate_fake_transit_data(period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2, 
+                       times, noiseLevel=None, ldtype='quadratic', transitType='primary'):
+  
+  cleanModel = batman_wrapper_lmfit(period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2, 
+                       times, ldtype='quadratic', transitType='primary')
+  
+  if noiseLevel is None:
+    noiseLevel = 1e-1
+   
+  noisyData = np.random.normal(cleanModel, noiseLevel)
+  dataError = np.random.normal(noiseLevel, 1e-2*noiseLevel)
+  return noisyData, dataError
+
+def batman_wrapper_mle(params, times, ldtype='quadratic', transitType='primary'):
+
+    period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2 = params
+    
+    if p0 == 1.0 and p1 == 0.0 and p2 == 0.0:
+        out_of_transit = 1.0
+    else:
+        out_of_transit = p0 + p1*(times - times.mean()) + p2*(times - times.mean())**2.
+    
+    bm_params           = batman.TransitParams() # object to store transit parameters
+
+    bm_params.per       = period  # orbital period
+    bm_params.t0        = tcenter # time of inferior conjunction
+    bm_params.inc       = inc     # inclunaition in degrees
+    bm_params.a         = aprs    # semi-major axis (in units of stellar radii)
+    bm_params.rp        = rprs    # planet radius (in units of stellar radii)
+    bm_params.ecc       = ecc     # eccentricity
+    bm_params.w         = omega   # longitude of periastron (in degrees)
+    bm_params.limb_dark = ldtype              # limb darkening model # NEED TO FIX THIS
+    bm_params.u         = [u1, u2]                  # limb darkening coefficients # NEED TO FIX THIS
+
+    m_eclipse = batman.TransitModel(bm_params, times, transittype=transitType)    # initializes model
+
+    return m_eclipse.light_curve(bm_params) * out_of_transit
+
+def batman_wrapper_lmfit(period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2, 
                        times, ldtype='quadratic', transitType='primary'):
 
     # period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0, p1, p2 = params
@@ -35,7 +73,7 @@ def batman_wrapper_raw(period, tcenter, inc, aprs, rprs, ecc, omega, u1, u2, p0,
     return m_eclipse.light_curve(bm_params) * out_of_transit
 
 def loglikehood(params, uni_prior, times, flux, fluxerr, regularization=None):
-    model = batman_wrapper_raw(params, times)
+    model = batman_wrapper_mle(params, times)
     chisq = ((flux - model)/fluxerr)**2.
     if regularization is None:
         return -0.5*chisq.sum() # + lambda*abs(params).sum() # + lambda*np.sqrt((params**2).sum())
@@ -69,10 +107,10 @@ u1In        = h11u1
 u2In        = h11u2
 offset      = 1.0
 slope       = 0.0
-quadterm    = 0.0
+curvature   = 0.0
 
 # Initial Parameters
-initParams = [periodIn, tcenterIn, incIn, aprsIn, rprsIn, eccIn, omegaIn, u1In, u2In]
+initParams = [periodIn, tcenterIn, incIn, aprsIn, rprsIn, eccIn, omegaIn, u1In, u2In, offset, slope, curvature]
 
 # Frozen Prior
 uniPrior = np.array([
@@ -100,7 +138,14 @@ uniPrior = np.array([
             [0.0,0.1] # uniform volume for u2
            ])
 
-res = optmin(neg_logprobability, initParams, args=(uniPrior, timeSlice3, fluxSlice3, ferrSlice3))
+nPts = 1000
+tSim = np.linspace(h11t0 - 0.2, h11t0 + 0.2, nPts)
+data, derr = generate_fake_transit_data(h11Per, h11t0, h11Inc, h11ApRs, h11RpRs, h11ecc, h11Omega, h11u1, h11u2, 
+                                        offset, slope, curvature, tSim, noiseLevel=0.1, 
+                                        ldtype='quadratic', transitType='primary')
+
+res = optmin(neg_logprobability, initParams, args=(uniPrior, tSim, data, derr))
+
 print(res.x - initParams) # it seems that the prior is overachieving -- maybe set the wrong init conditions
 
 ## FIRST TRY at LMFIT
@@ -120,21 +165,13 @@ p.add('h11u2', value = 0.048, vary=True)
 p.add('intercept', value = 1.0, vary=True)
 p.add('slope', value = 0.0, vary=True)
 p.add('curvature', value = 0.0, vary=True)
-'''
-df = pd.read_csv('w18.csv')
-t = df['time'].values
-orbit = df['orbit'].values
-scanD = df['scanD'].values
-f0 = df['flux0'].values
-fitLCModel(params, times, flux0, scanD, df['weights'].values)
-'''
-lc = Model(batman_wrapper_raw, independent_vars=['times', 'ldtype', 'transitType'])
+lc = Model(batman_wrapper_lmfit, independent_vars=['times', 'ldtype', 'transitType'])
 
-fitResult = lc.fit(f0,
-                   tExp=tExp,
+fitResult = lc.fit(data,
                    times=times,
                    ldtype='quadratic',
                    transitType='primary',
                    params=p,
                    method='powell')
+
 print(fitResult)
